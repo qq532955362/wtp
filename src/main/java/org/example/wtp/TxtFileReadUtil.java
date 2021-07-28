5536e67bc52270c4bc1458e28d485a9f41e8cae0
wangtaiping <532955362@qq.com>
2021-07-28 17:12:18 +0800

    refactor(文件): 增加mac下的文件导出

--- src/main/java/org/example/wtp/TxtFileReadUtil.java
+++ src/main/java/org/example/wtp/TxtFileReadUtil.java
@@ -14,6 +14,7 @@ public class TxtFileReadUtil {
         File file = new File(sourceFilepath);
 
         //暂时写死,如有必要可以传入
+//        File desFile = new File("/Users/wangtaiping/Desktop/phone.txt");
         File desFile = new File("D:\\phone.txt");
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -29,6 +30,7 @@ public class TxtFileReadUtil {
     }
 
     public static void main(String[] args) throws IOException {
+//        readPhoneFromTxt("/Users/wangtaiping/Desktop/记忆力主修课第2期用户-0720.txt");
         readPhoneFromTxt("C:\\Users\\Administrator\\Documents\\WXWork\\1688850835112175\\Cache\\File\\2021-07\\记忆力主修训练营2期.txt");
     }
 
