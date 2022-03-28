5114841c2750d8104d8b49b4a18dbc67e309164c
wangtaiping <532955362@qq.com>
2022-03-28 20:53:13 +0800

    feat(user): UserApplication初步接入

--- cloud/user/src/main/java/org/example/user/UserApplication.java
+++ cloud/user/src/main/java/org/example/user/UserApplication.java
@@ -1,11 +1,18 @@
 package org.example.user;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wangtaiping
  * 2022/3/8 14:32
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
+
+    public static void main(String[] args) {
+        new SpringApplication(UserApplication.class).run(args);
+    }
 }
