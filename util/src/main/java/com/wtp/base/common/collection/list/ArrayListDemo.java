85f88eadb61ffe734770c58e5d9692fa6cddf81e
wangtaiping <taiping.wang@ihoment.com>
2022-05-17 18:19:45 +0800

    feat(ArrayListDemo): 测试向ArrayList中添加null元素

---
+++ util/src/main/java/com/wtp/base/common/collection/list/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.wtp.base.common.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ArrayList可以添加null输出之后用的是直接是null字符串
+ *
+ */
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(null);
+        integerList.add(null);
+        integerList.add(1);
+        integerList.forEach(a-> System.out.println(a));
+    }
+}
