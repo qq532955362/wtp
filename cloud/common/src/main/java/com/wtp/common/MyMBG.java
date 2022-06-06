4bf414a04920fc93aa6a44bb0fc2abe72e60bca2
wangtaiping <taiping.wang@ihoment.com>
2022-06-06 12:06:06 +0800

    feature(补充淘宝地址): 所有含有多级行政区域的国家补充

--- cloud/common/src/main/java/com/wtp/common/MyMBG.java
+++ cloud/common/src/main/java/com/wtp/common/MyMBG.java
@@ -8,6 +8,6 @@ public class MyMBG {
     public static void main(String[] args) {
         MyGenerator myGenerator = new MyGenerator();
 //        myGenerator.generate(args[0], args[1], args[2], args[3]);
-        myGenerator.generate("cloud/user", "flat", "govee.app.mall.mapper.mall","");
+        myGenerator.generate("cloud/user", "flat", "org.example.user.pojo.dao","");
     }
 }
---
+++ cloud/user/src/main/java/org/example/user/pojo/assembler/AddressAssembler.java
@@ -0,0 +1,19 @@
+package org.example.user.pojo.assembler;
+
+import org.example.user.pojo.dao.dao.model.AppShippingAddress;
+import org.example.user.pojo.request.address.AddAddressRequest;
+import org.example.user.pojo.vo.AddressVO;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface AddressAssembler {
+
+    AddressVO requestToVO(AddAddressRequest addAddressRequest);
+
+    List<AddressVO> requestListToVOList(List<AddAddressRequest> addAddressRequestList);
+
+    AppShippingAddress requestToModel(AddAddressRequest addAddressRequest);
+
+}
