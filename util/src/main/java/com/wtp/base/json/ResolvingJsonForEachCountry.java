dd30be23a18f5d24ece518be05a7b5849a69e750
wangtaiping <taiping.wang@ihoment.com>
2022-06-06 17:38:01 +0800

    feature(补充淘宝地址): 所有含有多级行政区域的国家补充

---
+++ util/src/main/java/com/wtp/base/json/ResolvingJsonForEachCountry.java
@@ -0,0 +1,75 @@
+package com.wtp.base.json;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ResolvingJsonForEachCountry {
+
+
+    public static List<OutState> resolveAmerican(String filename) throws IOException {
+
+        String jsonString = ResolvingJson.readFileToJson(filename);
+        List<OutState> outStateList = JSONObject.parseArray(jsonString, OutState.class);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/Users/wangtaiping/IdeaProjects/open/resources/美国各州sql.sql"));
+        AtomicInteger flag = new AtomicInteger(236);
+        outStateList.forEach(a->{
+            try {
+                bufferedWriter.write("INSERT INTO `app_administration` ( `id`,`en_name`, `parent_id`) VALUES " +String.format(" ( '%d','%s','%s');", flag.get(),a.getName(),107));
+                bufferedWriter.newLine();
+                List<OutState.InnerCity> values = a.getValues();
+                values.forEach(innerCity -> {
+                    innerCity.getItems().forEach(city -> {
+                        try {
+                            bufferedWriter.write("INSERT INTO `app_administration` ( `en_name`, `parent_id`) VALUES " +String.format(" ( '%s','%s');",city.getName(),flag.get()));
+                            bufferedWriter.newLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            flag.incrementAndGet();
+        });
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        return outStateList;
+    }
+
+    @Data
+    public static class OutState {
+        private String name;
+        private List<InnerCity> values;
+        private Integer parentId;
+
+
+        @Data
+        public static class InnerCity {
+            private String label;
+            private List<City> items;
+
+            @Data
+            public static class City {
+                private String name;
+                private String id;
+                private Integer parentId;
+            }
+        }
+    }
+
+
+    @Test
+    public void testOut() throws IOException {
+        List<OutState> outStates = resolveAmerican("/Users/wangtaiping/IdeaProjects/open/resources/美国.json");
+    }
+
+
+}
